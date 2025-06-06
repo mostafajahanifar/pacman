0209c40c9c593f2aaa19737cec0b671300ef72fc
mostafajahanifar <mostafa.jahanifar@gmail.com>
2025-06-06 15:22:20 +0100

    methylation aggregation updated

--- pacman/genomic/methylation_results_aggregation.py
+++ pacman/genomic/methylation_results_aggregation.py
@@ -1,44 +1,32 @@
 import os
-import pandas as pd
-import numpy as np
-from openpyxl import Workbook
 
-# Load gene name mapping from data_methylation.txt
-# def load_gene_name_mapping(mapping_file):
-#     "Add CpG id to name for repeated cases"
-#     df = pd.read_csv(mapping_file, sep="\t")
-#     df['NAME'] = df.apply(lambda row: row['ENTITY_STABLE_ID'] if pd.isna(row['NAME']) else row['NAME'], axis=1)
-    
-#     # Create a set to keep track of seen names
-#     seen_names = set()
-    
-#     # Iterate over the DataFrame to update duplicate names
-#     for index, row in df.iterrows():
-#         name = row['NAME']
-#         if name in seen_names:
-#             df.at[index, 'NAME'] = f"{name}_{row['ENTITY_STABLE_ID']}"
-#         seen_names.add(df.at[index, 'NAME'])
-    
-#     return df.set_index('ENTITY_STABLE_ID')['NAME'].to_dict()
+import pandas as pd
+
+from pacman.config import ALL_CANCERS, DATA_DIR, RESULTS_DIR
+
+print(7*"="*7)
+print(f"Running Gene Methylation Results Aggregation")
+print(7*"="*7)
+
 
 def load_gene_name_mapping(mapping_file):
     """Add CpG id to all names"""
     df = pd.read_csv(mapping_file, sep="\t")
     df['NAME'] = df.apply(lambda row: row['ENTITY_STABLE_ID'] if pd.isna(row['NAME']) else row['NAME'], axis=1)
-    
+
     # Append ENTITY_STABLE_ID to each name
     # df['NAME'] = df.apply(lambda row: f"{row['NAME']}_{row['ENTITY_STABLE_ID']}", axis=1)
     df['NAME'] = df.apply(lambda row: f"{row['NAME']}", axis=1)
-    
+
     return df.set_index('ENTITY_STABLE_ID')['NAME'].to_dict()
 
 # Directory containing the cancer types' subdirectories
-base_dir = "results_final_all/gene/methylation"
+base_dir = f"{RESULTS_DIR}/genomic/methylation"
 # Create a writer to write results into an Excel file
-output_file = "results_final_all/gene/methylation/gene_methylation_aggregated.xlsx"
+output_file = f"{RESULTS_DIR}/genomic/methylation/gene_methylation_aggregated.xlsx"
 writer = pd.ExcelWriter(output_file, engine='openpyxl')
 
-mapping_file = "gene/data/data_methylation.txt"
+mapping_file = f"{DATA_DIR}/data_methylation.txt"
 
 # Load the gene ID to name mapping
 id_to_name = load_gene_name_mapping(mapping_file)
@@ -58,7 +46,7 @@ def process_correlation_cancer_type(cancer_type, corr_file, pval_file, writer, i
     if significant_genes.empty:
         print(f"No signicant gene for {cancer_type}")
         return -1
-    
+
     if len(significant_genes) >=5:
         corr_matrix = corr_matrix.loc[significant_genes]
         pval_matrix = pval_matrix.loc[significant_genes]
@@ -79,9 +67,7 @@ def process_correlation_cancer_type(cancer_type, corr_file, pval_file, writer, i
     # Reorder the matrices based on sorted genes
     corr_matrix = corr_matrix.loc[sorted_genes]
     pval_matrix = pval_matrix.loc[sorted_genes]
-    # # Rename the genes using the mapping dictionary (if the gene is found in the mapping)
-    # corr_matrix.index = [id_to_name.get(gene, gene) for gene in corr_matrix.index]
-    # pval_matrix.index = corr_matrix.index
+
     corr_matrix["NAME"] = [id_to_name.get(gene, gene) for gene in corr_matrix.index]
     pval_matrix["NAME"] = corr_matrix["NAME"]
     corr_matrix = corr_matrix[["NAME"]+list(corr_matrix.columns[:-1])]
@@ -89,7 +75,7 @@ def process_correlation_cancer_type(cancer_type, corr_file, pval_file, writer, i
 
     corr_matrix=corr_matrix.rename(index={"": "ID"})
     pval_matrix=pval_matrix.rename(index={"": "ID"})
-    
+
     # Format the correlation values: 2 decimals and add '*' if p-value < 0.05
     formatted_corr_matrix = corr_matrix.copy()
     for gene in corr_matrix.index:
@@ -108,12 +94,12 @@ def process_correlation_cancer_type(cancer_type, corr_file, pval_file, writer, i
 # Loop through each cancer type's directory
 for cancer_type in sorted(os.listdir(base_dir)):
     cancer_dir = os.path.join(base_dir, cancer_type)
-    
+
     if os.path.isdir(cancer_dir):
         print(cancer_type)
         corr_file = os.path.join(cancer_dir, f"corr_r.csv")
         pval_file = os.path.join(cancer_dir, f"corr_p.csv")
-        
+
         if os.path.exists(corr_file) and os.path.exists(pval_file):
             process_correlation_cancer_type(cancer_type, corr_file, pval_file, writer, id_to_name)
 
