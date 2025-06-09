cfd0594735900c388054f65cc12015366a2b19d4
mostafajahanifar <mostafa.jahanifar@gmail.com>
2025-06-09 11:19:27 +0100

    Updated immune correlation measuring

--- pacman/immune/correlation.py
+++ pacman/immune/correlation.py
@@ -1,110 +1,52 @@
+import os
+
+import matplotlib.pyplot as plt
 import pandas as pd
-import numpy as np
-from sklearn.preprocessing import StandardScaler
-from scipy import stats
-from utils import featre_to_tick, get_colors_dict
-import matplotlib.pyplot as plt
-
 import seaborn as sns
-import matplotlib.pyplot as plt
-from scipy import stats
-from statsmodels.stats.multitest import multipletests
 
-def calculate_corr_matrix(df1, df2, method='pearson', pvalue_correction="fdr_bh"):
-    if method not in ['spearman', 'pearson']:
-        raise ValueError("Method must be 'spearman' or 'pearson'")
-    
-    corr_matrix = pd.DataFrame(index=df1.columns, columns=df2.columns, dtype=np.float32)
-    pvalue_matrix = pd.DataFrame(index=df1.columns, columns=df2.columns, dtype=np.float32)
-    for row in df1.columns:
-        for col in df2.columns:
-            df_no_na = pd.concat([df1[row], df2[col]], axis=1)
-            df_no_na = df_no_na.dropna(axis=0, how="any")
-            if method == 'spearman':
-                corr, pvalue = stats.spearmanr(df_no_na[row], df_no_na[col])
-            elif method == 'pearson':
-                corr, pvalue = stats.pearsonr(df_no_na[row], df_no_na[col])
-            corr_matrix.at[row, col] = np.float32(corr)
-            pvalue_matrix.at[row, col] = np.float32(pvalue)
-    # correcting pvalues for the number of genes
-    if pvalue_correction is not None:
-        # Flatten the DataFrame to a 1D array
-        pvals = pvalue_matrix.values.flatten()
-        # Apply the correction
-        corrected_pvals = multipletests(pvals, alpha=0.05, method=pvalue_correction)[1]
-        # Reshape the corrected p-values back to the original shape of pvalue_matrix
-        corrected_pvals_matrix = corrected_pvals.reshape(pvalue_matrix.shape)
-        # Replace the values in the original DataFrame
-        pvalue_matrix.loc[:, :] = corrected_pvals_matrix
+from pacman.config import ALL_CANCERS, DATA_DIR, RESULTS_DIR
+from pacman.utils import calculate_corr_matrix
 
-    return corr_matrix, pvalue_matrix
+print(7 * "=" * 7)
+print("Running Correlation Analysis between Mitosis Features and Immune Features")
+print(7 * "=" * 7)
 
-ALL_CANCERS = ['SARC',
-    'LIHC',
-    'THYM',
-    'ACC',
-    'BRCA',
-    'KICH',
-    'STAD',
-    'BLCA',
-    'THCA',
-    'GBMLGG',
-    'UCEC',
-    'LUAD',
-    'KIRC',
-    'KIRP',
-    'PAAD',
-    'CESC',
-    'PCPG',
-    'MESO',
-    'SKCM',
-    'PRAD',
-    'COADREAD',
-    'ESCA',
-    'LUSC',
-    'HNSC',
-    'OV',
-    'TGCT',
-    'CHOL',
-    'DLBC',
-    'UCS'
- ]
-ALL_CANCERS = sorted(ALL_CANCERS)
-
+# Define the immune features to be used in the analysis
 immune_feats = [
-    "Proliferation", 
-                # "Th1 Cells","Th2 Cells","Th17 Cells", # "Th1/Th2 cell ratio",
-                # "NK Cells Activated","NK Cells Resting",#"NK Cells Resting/Activated ratio",
-                # "T Cells Regulatory Tregs",
-                # "B Cells Memory","B Cells Naive",
-                # "Dendritic Cells Activated","Dendritic Cells Resting",#"Dendritic Cells Resting/Activated ratio",
-                # "Monocytes","Macrophages M0","Macrophages M1","Macrophages M2", #"Macrophages M1/M2 ratio","Macrophages M2/M1 ratio",
-                # "Macrophages M2:Th1 ratio",
-                # "Mast Cells Activated","Mast Cells Resting", #"Mast Cells Resting/Activated ratio",
+                # selected immune features
+                "Th1 Cells","Th2 Cells","Th17 Cells",
+                "NK Cells Activated","NK Cells Resting",
+                "T Cells Regulatory Tregs",
+                "Monocytes","Macrophages M0","Macrophages M1","Macrophages M2",
+
+                ## Other possible immune features
+                # "Proliferation",
+                #  "Th1:Th2 cells ratio",
+                # "Dendritic Cells Activated","Dendritic Cells Resting",
+                # "Mast Cells Activated","Mast Cells Resting",
                 # "Plasma Cells",
-                # "T Cells CD4 Memory Activated","T Cells CD4 Memory Resting","T Cells CD4 Naive",#"T Cells CD4 Resting/Activated ratio",
+                # "T Cells CD4 Memory Activated","T Cells CD4 Memory Resting","T Cells CD4 Naive",
                 # "T Cells CD8",
                 # "T Cells Follicular Helper",
                 # "Lymphocytes","Neutrophils","Eosinophils","Mast Cells","Dendritic Cells","Macrophages"
                 ]
 
-# keep only columns that are related to mutations
-immune_df = pd.read_csv("gene/data/tcga_all_immune_new.csv")
-immune_df["TCGA Study"] = immune_df["TCGA Study"].replace(["COAD", "READ"], "COADREAD")
-immune_df["TCGA Study"] = immune_df["TCGA Study"].replace(["GBM", "LGG"], "GBMLGG")
-
 selected_feats = [
 "mean(ND)",
-"aty_wsi_ratio",
-"aty_ahotspot_count",
+"AFW",
+"AMAH",
 ]
-feat = "mean(ND)"
+feat = "mean(ND)" # the feature for which collaboration is done
 
-mitosis_feats = pd.read_csv('/mnt/gpfs01/lsf-workspace/u2070124/Data/Data/pancancer/tcga_features_final_ClusterByCancer_withAtypical.csv')
+#reading necessary data
+mitosis_feats = pd.read_excel(os.path.join(DATA_DIR, "ST1-tcga_mtfs.xlsx"))
 mitosis_feats = mitosis_feats[["bcr_patient_barcode", "type", "temperature"]+selected_feats]
-mitosis_feats.columns = [featre_to_tick(col) if col not in ["bcr_patient_barcode", "type", "temperature"] else col for col in mitosis_feats.columns]
-mitosis_feats["type"] = mitosis_feats["type"].replace(["COAD", "READ"], "COADREAD")
-mitosis_feats["type"] = mitosis_feats["type"].replace(["GBM", "LGG"], "GBMLGG")
+
+immune_df = pd.read_csv(os.path.join(DATA_DIR, "tcga_all_immune_new.csv"))
+# drop missing data
+immune_df = immune_df.dropna(axis=1, how="all")
+
+
 all_corr = []
 all_pval = []
 
@@ -112,27 +54,19 @@ for cancer_type in ALL_CANCERS + ["Mitotic Hot", "Mitotic Cold", "All"]:
 
     if cancer_type in ["Mitotic Hot", "Mitotic Cold", "All"]:
         mitosis_feats_cancer = mitosis_feats[mitosis_feats["type"].isin(ALL_CANCERS)]
-        gene_exp_cancer = immune_df[immune_df["TCGA Study"].isin(ALL_CANCERS)]
     else:
         mitosis_feats_cancer = mitosis_feats[mitosis_feats["type"]==cancer_type]
-        gene_exp_cancer = immune_df[immune_df["TCGA Study"]==cancer_type]
 
     if cancer_type=="Mitotic Hot":
         mitosis_feats_cancer = mitosis_feats_cancer[mitosis_feats_cancer["temperature"]=="Hot"]
     if cancer_type=="Mitotic Cold":
         mitosis_feats_cancer = mitosis_feats_cancer[mitosis_feats_cancer["temperature"]=="Cold"]
 
-
-    # drop missing mutations
-    gene_exp_cancer = gene_exp_cancer.dropna(axis=1, how="all")
-    # drop cases with all mutations as Nan
-
-
     # Find the common case names between mitosis features and gene expressions
-    common_cases = pd.Series(list(set(mitosis_feats_cancer['bcr_patient_barcode']).intersection(set(gene_exp_cancer['TCGA Participant Barcode']))))
+    common_cases = pd.Series(list(set(mitosis_feats_cancer['bcr_patient_barcode']).intersection(set(immune_df['TCGA Participant Barcode']))))
     ## Keep only the rows with the common case names in both dataframes
     df1_common = mitosis_feats_cancer[mitosis_feats_cancer['bcr_patient_barcode'].isin(common_cases)]
-    df2_common = gene_exp_cancer[gene_exp_cancer['TCGA Participant Barcode'].isin(common_cases)]
+    df2_common = immune_df[immune_df['TCGA Participant Barcode'].isin(common_cases)]
     df2_common = df2_common.drop_duplicates(subset='TCGA Participant Barcode')
 
     ## Sort the dataframes based on 'case_name'
@@ -143,7 +77,6 @@ for cancer_type in ALL_CANCERS + ["Mitotic Hot", "Mitotic Cold", "All"]:
     df2_common = df2_common.reset_index(drop=True)
 
     X = df1_common[[feat]]
-    # X = df2_common[[feat]]
     Y = df2_common[immune_feats]
     corr_matrix, pvalue_matrix = calculate_corr_matrix(Y, X, method='spearman', pvalue_correction="bonferroni")
 
@@ -152,8 +85,10 @@ for cancer_type in ALL_CANCERS + ["Mitotic Hot", "Mitotic Cold", "All"]:
 
 corr_matrix = pd.concat(all_corr, axis=1)
 pvalue_matrix = pd.concat(all_pval, axis=1)
-corr_matrix.to_csv(f"results_final_all/immune/{feat}_correlations_r.csv")
-pvalue_matrix.to_csv(f"results_final_all/immune/{feat}_correlations_p.csv")
+save_dir = os.path.join(RESULTS_DIR, "immune")
+os.makedirs(save_dir, exist_ok=True)
+corr_matrix.to_csv(f"{save_dir}/{feat}_correlations_r.csv")
+pvalue_matrix.to_csv(f"{save_dir}/{feat}_correlations_p.csv")
 
 annotations = pvalue_matrix.applymap(lambda x: '*' if x < 0.05 else '') 
 
@@ -191,5 +126,4 @@ for label in heatmap3.get_xticklabels():
     label.set_rotation(90)
 plt.subplots_adjust(wspace=0.03, hspace=0)
 
-plt.savefig(f"results_final_all/immune/{feat}_correlations_new.png", dpi=600, bbox_inches = 'tight', pad_inches = 0.01)
-# plt.savefig("results_final_all/immune/correlations.pdf", dpi=600, bbox_inches = 'tight', pad_inches = 0.01)
+plt.savefig(f"{save_dir}/{feat}_correlations_new.png", dpi=600, bbox_inches = 'tight', pad_inches = 0.01)
